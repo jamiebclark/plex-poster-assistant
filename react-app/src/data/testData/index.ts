309684d39707458ac4c45fc2d7db717b20c8490f
Jamie Desktop <jamiebclark@gmail.com>
2023-12-25 14:24:38 -0500

    Removes old React App folder

--- react-app/src/data/testData/index.ts
+++
@@ -1,9 +0,0 @@
-import { Poster } from '../../lib/loadPostersFromPage';
-import thePosterDb from './thePosterDb';
-
-const config = { thePosterDb }
-
-export default function loadTestData(key: keyof typeof config, setter: React.Dispatch<React.SetStateAction<Poster[]>>) {
-  const posters = config[key] as Poster[]
-  posters.forEach((poster) => setter((p) => [...p, poster]));
-}
